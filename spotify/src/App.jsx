8d98ec14f551e162709bb4b465b6f62b49dc9e33
Victor <victoranyadiegwu610@gmail.com>
2025-12-26 13:24:44 +0100

    Adapt for Vercel serverless deployment

--- spotify/src/App.jsx
+++ spotify/src/App.jsx
@@ -5,7 +5,7 @@ import MainContent from './components/maincontent/MainContent';
 import Player from './components/player/Player';
 import './App.css';
 
-const BACKEND_URL = 'http://127.0.0.1:5000';
+const BACKEND_URL = 'https://a-spotify-clone.vercel.app/';
 
 function getTokensFromUrl() {
   const hash = window.location.hash.substring(1);
